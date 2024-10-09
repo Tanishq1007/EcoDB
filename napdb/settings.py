7425975ac695a79c781537cba6430ae42bf6910c
Tanishq1007 <132736717+Tanishq1007@users.noreply.github.com>
2024-10-09 23:49:50 +0530

    Update settings.py

--- napdb/settings.py
+++ napdb/settings.py
@@ -143,4 +143,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True  # Allow all origins (for local development only)
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # If you're testing locally
+    'https://eco-assignment-gamma.vercel.app/',  # Production frontend URL
+]
+  # Allow all origins (for local development only)
